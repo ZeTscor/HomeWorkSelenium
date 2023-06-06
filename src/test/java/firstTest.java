f2d199f39fc4482a39a0a9bc32a981634c0d8af7
zets <poi.ru0130@gmail.com>
2023-06-06 13:29:01 +0300

    Initial commit

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class firstTest {
+
+
+    @Test
+    public void test() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://google.com");
+        driver.quit();
+
+    }
+
+}
